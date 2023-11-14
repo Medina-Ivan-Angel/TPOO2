6770d4e31568236bf0fcd4bd0d610c80daf87c03
Ivan Angel Medina <ivanangelmedina@gmail.com>
2023-11-14 16:35:01 -0300

    test commit para agregar las librerias

---
+++ TPPoo2/test/BorrameTest.java
@@ -0,0 +1,19 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class BorrameTest {
+
+	@BeforeEach
+	void setUp() throws Exception {
+		
+	}
+
+	@Test
+	void test() {
+		
+		fail("Not yet implemented");
+	}
+
+}
