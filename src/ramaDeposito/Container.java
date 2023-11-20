4b5a0560fa2b4f0b15b7c582e50b2da1ebd47955
Victor Bertolini <victor_bertolini_agaras@yahoo.com.ar>
2023-11-20 18:52:02 -0300

    Agregue los atributos a container y el constructor

--- src/ramaDeposito/Container.java
+++ src/ramaDeposito/Container.java
@@ -2,4 +2,18 @@ package ramaDeposito;
 
 public abstract class Container {
 
+	// Atributos.
+	private double ancho;
+	private double altura;
+	private double largo;
+	private double pesoTotal;
+	
+	// Constructor.
+	public Container(double ancho, double altura, double largo, double pesoTotal) {
+		this.altura = altura;
+		this.ancho = ancho;
+		this.largo = largo;
+		this.pesoTotal = pesoTotal;
+	}
+	
 }
