2e5082bd0c26da200b6d948c292d2a5c857c4375
Victor Bertolini <victor_bertolini_agaras@yahoo.com.ar>
2023-11-20 17:16:07 -0300

    Casi esta la clase Camion.

--- src/ramaDeposito/Camion.java
+++ src/ramaDeposito/Camion.java
@@ -2,4 +2,31 @@ package ramaDeposito;
 
 public class Camion {
 
+	// Atributos.
+	private Container carga;
+	private Chofer conductor;
+	private int nroDeSerie;
+	
+	// Constructor.
+	public Camion(Container carga, Chofer conductor, int nroDeSerie) {
+		this.carga = carga;
+		this.conductor = conductor;
+		this.nroDeSerie = nroDeSerie;
+	}
+	
+	// Metodos.
+		// Getters.
+	public Chofer getConductor() {
+		return(this.conductor);
+	}
+	
+	public int getNroDeSerie() {
+		return(this.nroDeSerie);
+	}
+	
+	public Container descargar() {
+        Container containerParaDescargar = this.carga;
+        this.carga = null; // el camión ya no tiene el contenedor
+        return containerParaDescargar;
+    }
 }
