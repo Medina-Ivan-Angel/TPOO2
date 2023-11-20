137bdc6c431e704db8ce47a865a09f5c7254e7d0
Victor Bertolini <victor_bertolini_agaras@yahoo.com.ar>
2023-11-20 15:15:34 -0300

    Empece Empresa, agregue el constructor, y el metodo getter del nombre.

--- src/ramaDeposito/EmpresaDeTransporte.java
+++ src/ramaDeposito/EmpresaDeTransporte.java
@@ -1,5 +1,25 @@
 package ramaDeposito;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class EmpresaDeTransporte {
 
+	// Atributos.
+	private List<Camion> flotaDeCamiones = new ArrayList<Camion>();
+	private List<Chofer> empleados = new ArrayList<Chofer>();
+	private String nombre;
+	
+	// Constructor.
+	public EmpresaDeTransporte(List<Camion> camiones, List<Chofer> choferes, String nombre) {
+		this.empleados = choferes;
+		this.flotaDeCamiones = camiones;
+		this.nombre = nombre;
+	}
+	
+	// Metodos.
+	public String getNombre() {
+		return(this.nombre);
+	}
+	
 }
