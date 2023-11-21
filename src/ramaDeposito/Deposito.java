c914a279957eb618a5fde3d26fb6ebf240c2bc84
Victor Bertolini <victor_bertolini_agaras@yahoo.com.ar>
2023-11-20 23:01:30 -0300

    Creados los atributos y el constructor.

--- src/ramaDeposito/Deposito.java
+++ src/ramaDeposito/Deposito.java
@@ -1,5 +1,21 @@
+// Package.
 package ramaDeposito;
+
+// Imports.
+import java.util.ArrayList;
+import java.util.List;
+import ramaCliente.Orden.Orden;
 
 public class Deposito {
 
+	// Atributos.
+	private List<Orden> ordenesActivas = new ArrayList<Orden>();
+	private List<Container> containers = new ArrayList<Container>();
+	
+	// Constructor.
+	public Deposito(List<Orden> ordenes, List<Container> carga) {
+		this.ordenesActivas = ordenes;
+		this.containers = carga;
+	}
+	
 }
