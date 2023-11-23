b30d5c49acab29425b6deb14c3349a8a6485b166
publico <publico@infor37b08.alu.unq.edu.ar>
2023-11-23 17:20:06 -0300

    Implementado Mockito

--- test/BorrameTestNuevo.java
+++ test/BorrameTestNuevo.java
@@ -1,17 +1,27 @@
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-class BorrameTestNuevo {
+import ramaCliente.Cliente;
+import ramaCliente.Consignee;
 
+
+class BorrameTestNuevo {
+	
+	Cliente cliente;
+	
+	Consignee consignee = mock(Consignee.class);
+	
 	@BeforeEach
 	void setUp() throws Exception {
 	}
 
 	@Test
 	void test() {
-		fail("Not yet implemented");
+		verify(consignee).getFechaYHoraDeLlegada();
 	}
 
 }
