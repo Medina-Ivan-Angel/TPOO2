998ee33669800abdd22b56f4891adfb50d4f7056
Ivan Angel Medina <ivanangelmedina@gmail.com>
2023-11-25 11:12:58 -0300

    Implementacion Mockito. Falta mas test.

--- test/BorrameTestNuevo.java
+++ test/BorrameTestNuevo.java
@@ -1,27 +1,45 @@
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
 
 import ramaCliente.Cliente;
 import ramaCliente.Consignee;
+import ramaDeposito.Dry;
 
 
 class BorrameTestNuevo {
 	
+	
+	
 	Cliente cliente;
 	
-	Consignee consignee = mock(Consignee.class);
+	@Mock
+	Dry mockDry = mock(Dry.class);
 	
 	@BeforeEach
 	void setUp() throws Exception {
+		
+		
+		
 	}
+	
+	
 
 	@Test
 	void test() {
-		verify(consignee).getFechaYHoraDeLlegada();
+		
+		when(mockDry.getAltura()).thenReturn(10.0);
+		
+		double alturaObtenida = mockDry.getAltura();
+		
+		verify(mockDry).getAltura();
+		
+		assertEquals(10.0, alturaObtenida);
 	}
 
 }
