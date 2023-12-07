c61d0a4c5a531aada529701bcf9bf291d4461ec6
Ivan Angel Medina <ivanangelmedina@gmail.com>
2023-12-07 09:52:37 -0300

    Commit final, no se pudo resolver el test terminalGestionada los estados de los buques no funcionan.

--- test/ramaNavieraCircuitosTest/ViajeTest.java
+++ test/ramaNavieraCircuitosTest/ViajeTest.java
@@ -3,6 +3,7 @@ package ramaNavieraCircuitosTest;
 import static org.junit.jupiter.api.Assertions.*;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -19,7 +20,7 @@ class ViajeTest {
 
 	// Mock Stub:
 	@Mock LocalDate fechaDeSalida;
-	
+	@Mock LocalDateTime fechaDeLlegada;
 	// Mock Fake objects:
 	
 	@Mock Circuito circuitoARecorrer;
@@ -33,16 +34,22 @@ class ViajeTest {
 		// Inicializo Mock Stub:
 		
 		fechaDeSalida = mock(LocalDate.class);
+		fechaDeLlegada = mock(LocalDateTime.class);
 		
 		// establezco comportamiento de este stub:
 		
 		when(fechaDeSalida).thenReturn(LocalDate.of(2023, 12, 8));
-		
+	
 		// Inicializo Fake objects:
 		
 		circuitoARecorrer = mock(Circuito.class);
 		terminalDestino = mock(TerminalNormal.class);
-		
+		/*
+		 * LocalDateTime fechaDeSalida,
+		 * Circuito circuitoARecorrer, 
+		   TerminalNormal terminalDestino,
+		   LocalDateTime fechaLlegadaADestino
+		 */
 		viaje = new Viaje(fechaDeSalida, circuitoARecorrer, terminalDestino);
 	
 	}
